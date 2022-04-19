fc5194c74237ceefc2bafef76b3c05a1f1ea8804
Dechev Daniel Ivanov <90359518+datas2k@users.noreply.github.com>
2022-04-19 08:00:38 +0200

    uramisten

---
+++ API/control/landing.js
@@ -0,0 +1,93 @@
+const ejs = require('ejs');
+const db = require('../model/model-mysql');
+
+exports.GET_game = (req,res)=>{
+    set_userGamePath(req,res,'game')
+}
+
+exports.POST_game = (req,res)=>{
+    req.session.room = req.body.gameroom;
+    req.session.game = req.body.gameroom;
+    res.redirect('/game')
+}
+
+
+exports.GET_lobby = (req,res)=>{
+    set_userPath(req,res,'lobby')
+}
+
+exports.POST_lobby = (req,res)=>{
+    req.session.game = 'null';
+    res.redirect('/lobby')
+}
+
+exports.GET_highscore = (req,res)=>{
+    const userInfo = getUserInfo(req);
+    req.session.route = 'highscore';
+    userInfo.route = req.session.route;
+    db.query(`SELECT username , score  , playedGames  FROM users ORDER BY score DESC;`, (err,results)=>{
+        if (err) throw err;
+        ejs.renderFile(`./API/view/landing/highscore.ejs`, {userInfo,results} , (err, data)=>{
+            if (err) throw err;
+            res.send(data);
+        });
+    });
+}
+
+exports.GET_help = (req,res)=>{
+    set_userPath(req,res,'help')
+}
+
+exports.GET_about = (req,res)=>{
+    set_userPath(req,res,'about')
+}
+
+function set_userPath(req,res,path) {
+    const userInfo = getUserInfo(req);
+    req.session.room = path;
+    req.session.route = path;
+    userInfo.room = path;
+    userInfo.route = path;
+    db.query(`UPDATE rooms SET room='${path}', route='${path}' WHERE userID=${req.session.userID};`, (err)=>{
+        if (err) throw err;
+        ejs.renderFile(`./API/view/landing/${path}.ejs`, {userInfo} , (err, data)=>{
+            if (err) throw err;
+            res.send(data);
+        });
+    });
+}
+
+function set_userGamePath(req,res,path) {
+    const userInfo = getUserInfo(req);
+    if (req.session.game == 'null') {
+       path = 'lobby'
+    }
+    req.session.route = path;
+    userInfo.route = req.session.route;
+    db.query(`UPDATE rooms SET route='${path}' WHERE userID=${req.session.userID};`, (err)=>{
+        if (err) throw err;
+        ejs.renderFile(`./API/view/landing/${path}.ejs`, {userInfo} , (err, data)=>{
+            if (err) throw err;
+            res.send(data);
+        });
+    });
+}
+
+function getUserInfo(req){
+    var userInfo = {
+        sessionID: req.session.id,
+        userID: req.session.userID,
+        name: req.session.username,
+        room: req.session.room,
+        route: req.session.route,
+        playerIndex: req.session.playerIndex 
+        
+    };
+    if (req.session.game) {
+        userInfo.game = req.session.game;
+    }
+    if (req.session.socket) {
+        userInfo.socket = req.session.socket;
+    }
+    return userInfo
+} 
---
+++ API/router.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('./control/auth.js');
+const landing = require('./control/landing.js');
+
+const redirectLogin = (req,res, next) => {
+    if (!req.session.userID) {
+        res.redirect('/login')
+    } else {
+        next();
+    }
+}
+
+const redirectLanding = (req,res, next) => {
+    if (req.session.userID) {
+        res.redirect(`/${req.session.route}`);
+    } else {
+        next();
+    }
+}
+
+router.get('/', auth.GET_root);
+router.get('/login',redirectLanding, auth.GET_login);
+router.post('/login', auth.POST_login); //redirectLanding
+router.get('/register', auth.GET_register);
+router.post('/register', auth.POST_register); // redirectLanding
+router.post('/logout', redirectLogin, auth.POST_logout);
+router.get('/sessionOccupied', auth.GET_sessionOccupied);
+
+router.get('/lobby', redirectLogin, landing.GET_lobby);
+router.post('/lobby', redirectLogin, landing.POST_lobby);
+router.get('/highscore', redirectLogin, landing.GET_highscore);
+router.get('/help', redirectLogin, landing.GET_help);
+router.get('/about', redirectLogin, landing.GET_about);
+
+router.get('/game', redirectLogin, landing.GET_game);
+router.post('/game', redirectLogin, landing.POST_game);
+
+module.exports = router;
